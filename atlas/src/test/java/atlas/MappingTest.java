6892b581e24d8157444abbcbd7fee431119de185
Spencer Kesse <spencerkesse@gmail.com>
2026-04-03 18:13:45 +0100

    Unit Tests for Mapping of 2 flat strings

---
+++ atlas/src/test/java/atlas/MappingTest.java
@@ -0,0 +1,47 @@
+package atlas;
+
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MappingTest {
+    // ---------User Story 2.2 Tests---------
+    @Test
+    public void testMismatchedLengthThrows() {
+        Mapping mapping = new Mapping();
+
+        String flat1 = "(serve *priest (some congregation))";
+        String flat2 = "(serve *soldier (some army (extra)))"; // more tokens than the other string
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            mapping.mapTwoFlatStrings(flat1, flat2);
+        });
+    }
+
+    @Test
+    public void testNestedMapping() {
+        Mapping mapping = new Mapping();
+        String flat1 = "(serve *priest (some congregation (that (perform (for (some god)) (some worship)))))";
+        String flat2 = "(serve *soldier (some army (that (perform (for (some leader)) (some conquest)))))";
+
+        HashMap<String, String> result = mapping.mapTwoFlatStrings(flat1, flat2);
+
+        assertEquals(4, result.size());
+        assertEquals("*priest", result.get("*soldier"));
+        assertEquals("congregation", result.get("army"));
+        assertEquals("god", result.get("leader"));
+        assertEquals("worship", result.get("conquest"));
+    }
+
+    @Test
+    public void testInvalidMappingThrows() {
+        Mapping mapping = new Mapping();
+        String flat1 = "(serve *priest)";
+        String flat2 = "(serve priest)"; // missing asterisk
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            mapping.mapTwoFlatStrings(flat1, flat2);
+        });
+    }
+}
