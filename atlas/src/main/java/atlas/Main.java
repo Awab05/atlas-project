02aea6287f403367719e32b17bdb14259919fb52
Awab <awabkashir11@gmail.com>
2026-04-11 19:30:43 +0100

    Completed User Story 3.1.

--- atlas/src/main/java/atlas/Main.java
+++ atlas/src/main/java/atlas/Main.java
@@ -1,4 +1,14 @@
 package atlas;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        RewriteRuleLoader loader = new RewriteRuleLoader();
+        loader.loadRules();
+
+        System.out.println(loader.getRules().get("dislike"));
+        System.out.println(loader.getRules().get("undermine"));
+        System.out.println(loader.getRules().get("flunk"));
+    }
 }
