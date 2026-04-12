51a03037d62d87307ae5a4e8bb506e979820b068
Awab <awabkashir11@gmail.com>
2026-04-12 21:28:07 +0100

    Completed unit tests for User Story 3.1.

--- atlas/src/test/java/atlas/RulesRewriteTest.java
+++ atlas/src/test/java/atlas/RulesRewriteTest.java
@@ -1,17 +1,71 @@
 package atlas;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import atlas.Rules.RewriteRuleLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import atlas.Rules.RewriteStructure;
 
-public class RulesRewriteTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+public class RulesRewriteTest {
+    // -----------------Story 3.1 Tests--------------
+
+    @Test
+    void testLoadRulesFileNotEmpty() throws IOException {
+        RewriteRuleLoader loader = new RewriteRuleLoader();
+        loader.loadRules();
+
+        assertFalse(loader.getRules().isEmpty());
+    }
+
+    @Test
+    void testLoadSingleRewriteRule() throws IOException {
+        RewriteRuleLoader loader = new RewriteRuleLoader();
+        loader.loadRules();
+
+        assertTrue(loader.getRules().containsKey("dislike"));
+        assertEquals(1, loader.getRules().get("dislike").size());
+        assertEquals("!respect_as:friend&disliking", loader.getRules().get("dislike").get(0));
+    }
+
+    @Test
+    void testLoadMultipleRewriteRules() throws IOException {
+        RewriteRuleLoader loader = new RewriteRuleLoader();
+        loader.loadRules();
+
+        assertTrue(loader.getRules().containsKey("undermine"));
+        assertEquals(2, loader.getRules().get("undermine").size());
+        assertEquals("!support_as:leader&undermining", loader.getRules().get("undermine").get(0));
+        assertEquals("!respect_as:leader&undermining", loader.getRules().get("undermine").get(1));
+    }
+
+    @Test
+    void testLoadAnotherMultipleRewriteRule() throws IOException {
+        RewriteRuleLoader loader = new RewriteRuleLoader();
+        loader.loadRules();
+
+        assertTrue(loader.getRules().containsKey("flunk"));
+        assertEquals(2, loader.getRules().get("flunk").size());
+        assertEquals("^!respect_for:teacher&flunking", loader.getRules().get("flunk").get(0));
+        assertEquals("^reject_for:teacher&flunking", loader.getRules().get("flunk").get(1));
+    }
+
+    @Test
+    void testKnownPredicateExists() throws IOException {
+        RewriteRuleLoader loader = new RewriteRuleLoader();
+        loader.loadRules();
+
+        assertTrue(loader.getRules().containsKey("exercise"));
+    }
+
+
+    //----------------story 3.2 tests-------------------------
     private RewriteStructure rewriteStructure;
 
     @BeforeEach
